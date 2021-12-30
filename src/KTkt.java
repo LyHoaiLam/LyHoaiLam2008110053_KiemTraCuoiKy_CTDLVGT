4bf8c384ad306358ce964e0c4ce71db53610fe36
Your Name <hoailam03999@gmail.com>
2021-12-30 23:56:18 +0700

    len

--- src/KTkt.java
+++ src/KTkt.java
@@ -2,6 +2,6 @@ public class KTkt
 {
     public static void main(String[] args) 
     {
-        System.out.println("fbskbfskbsks");    
+        System.out.println("Ly Hoai Lam");    
     }    
 }
